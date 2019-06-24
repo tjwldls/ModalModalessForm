205568bb20cb4bc672e77e7ef43cf8fa83c8192b
Jiin Seo <s2017i28@e-mirim.hs.kr>
2019-06-24 13:51:59 +0900

    #1 Modal & Modaless 창 띄우기 by 3109 서지인

--- ModalModalessForm/FormAbout.Designer.cs
+++ ModalModalessForm/FormAbout.Designer.cs
@@ -48,7 +48,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("타이포_꼬맹이마음 M", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(51, 294);
+            this.label1.Location = new System.Drawing.Point(51, 295);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(193, 39);
             this.label1.TabIndex = 1;
